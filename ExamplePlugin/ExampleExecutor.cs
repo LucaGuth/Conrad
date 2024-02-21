9883c1d60b449af3e70d5bf9c9a1bcdc0c374fda
MaZe3D <45102464+MaZe3D@users.noreply.github.com>
2024-02-21 09:34:25 +0100

    create ExamplePluginPackage

---
+++ ExamplePlugin/ExampleExecutor.cs
@@ -0,0 +1,14 @@
+﻿using PluginInterfaces;
+
+namespace ExamplePlugin
+{
+    public class ExampleExecutor : IExecutorPlugin
+    {
+        public string Name { get => nameof(ExampleExecutor); }
+
+        public string Execute(string parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExamplePlugin/ExampleNotifier.cs
@@ -0,0 +1,14 @@
+﻿using PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePluginPackage
+{
+    internal class ExampleNotifier : INotifierPlugin
+    {
+        public string Name => throw new NotImplementedException();
+    }
+}
