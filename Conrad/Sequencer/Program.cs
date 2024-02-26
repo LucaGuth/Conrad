405279fb6826e84b4cfd9e3eab5f393f4837c483
Benedikt Hauser <inf21037@lehre.dhbw-stuttgart.de>
2024-02-26 15:39:45 +0100

    add serilog to sequencer

--- Conrad/Sequencer/Program.cs
+++ Conrad/Sequencer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Sequencer;
+using Serilog;
 using System.CommandLine;
 
 namespace scl;
@@ -7,37 +8,57 @@ class Program
 {
     static async Task<int> Main(string[] args)
     {
-        var conradBasePath   = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".conrad");
-        var conradConfigPath = Path.Combine(conradBasePath, "config.json");
-        var conradPluginPath = Path.Combine(conradBasePath, "plugins");
+        // Setup the logger 
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt")
+            .CreateLogger();
+        Log.Information("Starting the program");
 
-        var pluginFolder = new Option<string?>(
-            name: "--plugins",
-            description: "The folder to load all plugins from.");
-        pluginFolder.AddAlias("-p");
-        pluginFolder.SetDefaultValue(conradPluginPath);
+            try{
+            var conradBasePath   = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".conrad");
+            var conradConfigPath = Path.Combine(conradBasePath, "config.json");
+            var conradPluginPath = Path.Combine(conradBasePath, "plugins");
 
-        var configFile = new Option<string?>(
-            name: "--config",
-            description: "The configuration for the plugins");
-        configFile.AddAlias("-c");
-        configFile.SetDefaultValue(conradConfigPath);
+            var pluginFolder = new Option<string?>(
+                name: "--plugins",
+                description: "The folder to load all plugins from.");
+            pluginFolder.AddAlias("-p");
+            pluginFolder.SetDefaultValue(conradPluginPath);
 
-        var generateConfig = new Option<bool>(
-            name: "--generate-config",
-            description: "Generate or update the current configuration file based on the current set of plugins. The Program will not start the sequencer and exit immediately after generating the configuration file.");
+            var configFile = new Option<string?>(
+                name: "--config",
+                description: "The configuration for the plugins");
+            configFile.AddAlias("-c");
+            configFile.SetDefaultValue(conradConfigPath);
+
+            var generateConfig = new Option<bool>(
+                name: "--generate-config",
+                description: "Generate or update the current configuration file based on the current set of plugins. The Program will not start the sequencer and exit immediately after generating the configuration file.");
+                configFile.SetDefaultValue(false);
             configFile.SetDefaultValue(false);
-        configFile.SetDefaultValue(false);
 
-        var rootCommand = new RootCommand("Conrad - Cognitive Optimizer for Notifications, Recommendations and Automated Data management");
+            var rootCommand = new RootCommand("Conrad - Cognitive Optimizer for Notifications, Recommendations and Automated Data management");
 
-        rootCommand.AddOption(pluginFolder);
-        rootCommand.AddOption(configFile);
-        rootCommand.AddOption(generateConfig);
+            rootCommand.AddOption(pluginFolder);
+            rootCommand.AddOption(configFile);
+            rootCommand.AddOption(generateConfig);
 
-        rootCommand.SetHandler(RunProgram!, configFile, pluginFolder, generateConfig);
+            rootCommand.SetHandler(RunProgram!, configFile, pluginFolder, generateConfig);
 
-        return await rootCommand.InvokeAsync(args);
+            return await rootCommand.InvokeAsync(args);
+        }
+        catch(Exception ex)
+        {
+            Log.Error(ex, "An error occurred");
+            return 1;
+        }
+        finally
+        {
+            Log.Information("Shutting down the program");
+            Log.CloseAndFlush();
+        }
     }
 
     internal static void RunProgram(string configFile, string pluginPath, bool generateConfig)
