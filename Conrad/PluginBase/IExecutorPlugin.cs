5b84edfc62198e383971412c7a3ceded92a42b4d
MaZe <45102464+MaZe3D@users.noreply.github.com>
2024-02-29 10:54:13 +0100

    add documentation commands

--- Conrad/PluginBase/IExecutorPlugin.cs
+++ Conrad/PluginBase/IExecutorPlugin.cs
@@ -1,8 +1,17 @@
 ﻿namespace PluginInterfaces
 {
+    /// <summary>
+    /// The interface for plugins that are able to execute commands.
+    /// </summary>
     public interface IExecutorPlugin : IPlugin
     {
         public string ParameterFormat { get; }
+
+        /// <summary>
+        /// The method that is called once the plugin needs to execute a command.
+        /// </summary>
+        /// <param name="parameter">The parameters needed for processing the request in YAML format.</param>
+        /// <returns>The result of the request.</returns>
         public string Execute(string parameter);
     }
 }
