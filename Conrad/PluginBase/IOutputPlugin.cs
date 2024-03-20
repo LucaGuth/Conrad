7b3be1dfeee92601ccd65fbc1585b0110fd85e3a
MaZe <45102464+MaZe3D@users.noreply.github.com>
2024-03-20 10:32:52 +0100

    add new interface OutputPluign

---
+++ Conrad/PluginBase/IOutputPlugin.cs
@@ -0,0 +1,21 @@
+﻿using PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBase
+{
+    /// <summary>
+    /// The interface for output plugins.
+    /// </summary>
+    public interface IOutputPlugin : IPlugin
+    {
+        /// <summary>
+        /// The method that is called to push a message to the output.
+        /// </summary>
+        /// <param name="message">The message that will be sent to the client.</param>
+        void PushMessage(string message);
+    }
+}
