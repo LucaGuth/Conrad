46a1dc8e205ccc695712d2bc9719787a6ef7661f
BZ <127404282+inf21064@users.noreply.github.com>
2024-04-09 18:03:22 +0200

    Clean up. Bugfix. Improved extraction logic for the input parameter.

--- Conrad/Plugins/WeatherPlugin/WeatherPlugin.cs
+++ Conrad/Plugins/WeatherPlugin/WeatherPlugin.cs
@@ -2,10 +2,9 @@
 using System.Net;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using Microsoft.CSharp.RuntimeBinder;
+using System.Text.RegularExpressions;
 using PluginInterfaces;
 using Serilog;
-using WeatherPlugin.PluginModels;
 
 namespace WeatherPlugin;
 
@@ -13,8 +12,7 @@ public class WeatherPlugin : IExecutorPlugin, IConfigurablePlugin
 {
     private readonly HttpClient _httpClient = new();
 
-    public string Name => "Weather Forecast";
-    private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
+    public string Name => "Weather Forecast Provider";
 
     public string Description =>
         "This plugin returns a weather forecast for a location, e.g. a city.";
@@ -24,7 +22,8 @@ public class WeatherPlugin : IExecutorPlugin, IConfigurablePlugin
         Log.Debug("Start execution of the WeatherPlugin");
         try
         {
-            var forecastString =  await GetWeatherForecastAsync(parameter);
+            var city = ExtractCity(parameter);
+            var forecastString =  await GetWeatherForecastAsync(city);
             var forecast = ParseAndFormatResponse(forecastString);
             return string.Join("\n", forecast);
         }
@@ -43,19 +42,45 @@ public class WeatherPlugin : IExecutorPlugin, IConfigurablePlugin
         }
     }
 
+    private string ExtractCity(string parameter)
+    {
+        string city;
+        const string pattern = @"(?:ForecastCity:)?['\{](?<city>[^'\}]+)['\}]";
+        char[] charsToTrim = ['{', '}', '*', ',', '.', ' ', '\''];
+
+        var regex = new Regex(pattern);
+        var match = regex.Match(parameter);
+
+        if (match.Success)
+        {
+            city = match.Groups["city"].Value.Trim(charsToTrim);
+        }
+        else
+        {
+            Log.Warning("The city could not be extracted from the input parameter. The whole parameter will be used as the city.");
+            city = parameter.Trim(charsToTrim);
+        }
+        Log.Debug("[{PluginName}] Parsed parameter: ForecastCity: {City}",
+            nameof(WeatherPlugin), city);
+        return city;
+    }
+
     private IEnumerable<string> ParseAndFormatResponse(string forecastString)
     {
         using var doc = JsonDocument.Parse(forecastString);
         var list = doc.RootElement.GetProperty("list").EnumerateArray();
 
         var forecast = (from item in list
-                let dateTimeText = item.GetProperty("dt_txt").GetString()
-                let time = DateTime.Parse(dateTimeText).TimeOfDay
+                let fullDateTimeText = item.GetProperty("dt_txt").GetString()
+                let dateTime = DateTime.ParseExact(fullDateTimeText, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+                let dateTimeText = dateTime.ToString("yyyy-MM-dd HH:mm")
+                let time = dateTime.TimeOfDay
                 let temperature = item.GetProperty("main").GetProperty("temp").GetDecimal()
                 let weatherDescription = item.GetProperty("weather")[0].GetProperty("description").GetString()
                 let windSpeed = item.GetProperty("wind").GetProperty("speed").GetDecimal()
                 where time == new TimeSpan(9, 0, 0) || time == new TimeSpan(18, 0, 0)
-                select $"{dateTimeText} - Temp: {temperature}°C, Description: {weatherDescription}, Wind Speed: {windSpeed} m/s")
+                select $"Weather forecast for the {dateTimeText} - A temperature of {temperature}°C with " +
+                       $"{weatherDescription} and a wind speed of {windSpeed} m/s.")
             .Take(4)
             .ToList();
 
