935bb8322fa2a55825f23d75ea254776f20cb7bc
Benedikt Hauser <inf21037@lehre.dhbw-stuttgart.de>
2024-04-16 12:12:08 +0200

    add dishplugin valid response test

--- test/DishPluginTest.cs
+++ test/DishPluginTest.cs
@@ -12,19 +12,49 @@ using System.Text.RegularExpressions;
 using FoodPlugin;
 using RichardSzalay.MockHttp;
 using System.Net;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
 
 [TestClass]
 public class DishPluginTest
 {
-    // [TestMethod]
-    // public async Task ValidResponseShouldBeParsed()
-    // {
-    //     // Arrange
-    
-    //     // Act
+    [TestMethod]
+    public async Task ValidResponseShouldBeParsed()
+    {
+        // add serilog console log
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-    //     // Assert
-    // }
+
+        // Arrange
+        var parameter = "dish:'pasta', cuisine:'italian'";
+        var expectedString = "For the dish 'BLT Pizza' one needs the following ingredients: shredded colby jack cheese, fat free light cream cheese, garlic powder, lettuce, pizza crust, light ranch dressing, diced tomato, cooked turkey bacon"; 
+
+        var jsonFilePath1 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "dish_example_1.json");
+        var jsonContent1 = File.ReadAllText(jsonFilePath1);
+        var jsonResponse1 = JObject.Parse(jsonContent1);
+        var jsonFilePath2 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "dish_example_2.json");
+        var jsonContent2 = File.ReadAllText(jsonFilePath2);
+        var jsonResponse2 = JObject.Parse(jsonContent2);
+    
+        var mockHttp = new MockHttpMessageHandler();
+        mockHttp.When("https://api.spoonacular.com/recipes/complexSearch*")
+            .Respond("application/json", jsonContent1);
+        mockHttp.When("https://api.spoonacular.com/recipes/680975/*")
+            .Respond("application/json", jsonContent2);
+
+        var _dishPlugin = new DishPlugin();
+        _dishPlugin.InjectHttpClient(mockHttp.ToHttpClient());
+        
+        // Act
+        string result = await _dishPlugin.ExecuteAsync(parameter);
+        Console.WriteLine(result);
+        // Assert
+        Assert.IsTrue(result.Contains(expectedString));
+    }
 
     [TestMethod]
     public async Task EmptyResponseShouldNotBeParsed()
