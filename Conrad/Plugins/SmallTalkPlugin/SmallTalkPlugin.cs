b4feebe35003849693a5f1f3f3bb37eced27a058
MaZe3D <45102464+MaZe3D@users.noreply.github.com>
2024-04-17 21:06:09 +0200

    create smalltalk-plugin

---
+++ Conrad/Plugins/SmallTalkPlugin/SmallTalkPlugin.cs
@@ -0,0 +1,18 @@
+﻿using PluginInterfaces;
+
+namespace SmallTalkPlugin
+{
+    public class SmallTalkPlugin : IExecutorPlugin
+    {
+        public string ParameterFormat => @"usermessage:'{message}'";
+
+        public string Name => "SmallTalkPlugin";
+
+        public string Description => "Responds to Smalltalk like Greetings, Thank you and How are you.";
+
+        public Task<string> ExecuteAsync(string parameter)
+        {
+            return Task.FromResult<string>("Respond polite and short to the origial user request.");
+        }
+    }
+}
